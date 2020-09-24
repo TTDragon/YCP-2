1f1b81401fb402814137e2d502765790c2af2f5d
TTDragon <68650916+TTDragon@users.noreply.github.com>
2020-09-24 21:17:56 +0300

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace usr2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            string b = Console.ReadLine();
+            int c, h;
+            c = Convert.ToInt32(a);
+            h = Convert.ToInt32(b);
+            if ((c > h) | (c<h) & (c!=h))
+            { Console.WriteLine(c>h);
+            }
+
+                
+            else 
+            
+                Console.WriteLine("Равны между собой");
+            
+           
+            
+                
+        }
+    }
+}
